9f3a58967290bc35b4114dafe7b58c38265e5534
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-03-17 16:06:26 -0300

    navbar ok

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
-var gulp = require('gulp'); 
+var gulp = require('gulp');
+var path = require('path');
 
 gulp.task('bootstrap', function(){
-	gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
+	gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css')
    		.pipe(gulp.dest('./public/stylesheets'));
 
-   	gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
+   	gulp.src('./bower_components/bootstrap/dist/js/bootstrap.min.js')
    		.pipe(gulp.dest('./public/javascripts'));
 
-   	gulp.src('./node_modules/bootstrap/dist/fonts/**')
+   	gulp.src('./bower_components/bootstrap/dist/fonts/*.*')
    		.pipe(gulp.dest('./public/fonts'));
 
     console.log('bootstrap');
 });
+
+
 
 
 
--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,11 +1,16 @@
-$(document).ready(function(){
-$(".panelToggle").click(function(){
-    $(".userPanel").toggleClass("width200px width45px");
-	$("i",this).toggleClass("fa-arrow-left fa-arrow-right");
-	$(".userLogo img").toggleClass("width50p width90p");
-	$(".userNameSpan").toggleClass("show hide");
-	$(".userMenu span").toggleClass("show hide");
-	$(".userMenu ul").toggleClass("showI2");
-	$(".userMenu ul i").toggleClass("Icon Icon2");
-});
+
+$(document).ready(function () {
+	$(function(){
+		$(".dropdown").hover(
+			function() {
+				$('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+				$(this).toggleClass('open');
+				$('b', this).toggleClass("caret caret-up");
+			},
+			function() {
+				$('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+				$(this).toggleClass('open');
+				$('b', this).toggleClass("caret caret-up");
+			});
+	});
 });

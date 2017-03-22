4ada3a1813e549abf0f718b8e7a408d7ff473611
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-03-22 15:14:49 -0300

    chamando json para o site

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var file = require('./../controller/file');
 
-router.get('/users', function (req, res){
+router.get('/', function (req, res){
 	fs.readFile(__dirname + '/../db/quadros.json', 'utf8', function(err, data){
-			console.log(data);
-			res.end(data);
+	data = JSON.parse(data);
+		console.log(data);
+	res.render('catalogo', { dados:data });
+			res.end();
 	});
 
 });

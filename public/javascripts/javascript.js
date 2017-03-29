1b8296edcee11e714bb44fcd5e40ca20782bf556
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-03-29 16:15:58 -0300

    rota dados

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,4 +1,4 @@
-
+var server = 'http://localhost:45000/dados';
 $(document).ready(function () {
 	$(function(){
 		$(".dropdown").hover(
@@ -16,8 +16,36 @@ $(document).ready(function () {
 	});
 
 	$('.myCarousel').carousel({
-        interval: 3000
-    });
+		interval: 3000
+	});
+
+	$('#txt-search').keyup(function(){
+	    $.get(server, function(data) {       
+	    	var searchField = $(this).val();
+			if(searchField == '')  {
+				$('#filter-records').html('');
+				return;
+			}
+			var regex = new RegExp(searchField, "i");
+			var output = '<div class="row">';
+			var count = 1;
+	  
+        	$(data).each(function(){
+			   	console.log(this);
+
+				if (this.Nome.search(regex) != -1){
+					output += '<div class="col-md-6 well">';
+					output += '<div class="col-md-3"><img class="img-responsive" src="" /></div>';
+					output += '<div class="col-md-7">';
+					output += '<h5>' + this.Nome + '</h5>';
+					output += '<p>' + this.Código + '</p>'
+					output += '</div>';
+					output += '</div>';
+				}
+			});
+		
+		output += '</div>';
+		$('#filter-records').html(output);
+		});
+	});
 });
-
-

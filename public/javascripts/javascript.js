d59ee30fefb419026cf008f607de0a9d2c558c4e
Bianca Tainá Duarte <bianca.duarte@digitaldesk.com.br>
2017-03-29 16:57:39 -0300

    search bar sendo feita com for

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -19,33 +19,53 @@ $(document).ready(function () {
 		interval: 3000
 	});
 
-	$('#txt-search').keyup(function(){
-	    $.get(server, function(data) {       
-	    	var searchField = $(this).val();
-			if(searchField == '')  {
-				$('#filter-records').html('');
-				return;
-			}
-			var regex = new RegExp(searchField, "i");
-			var output = '<div class="row">';
-			var count = 1;
-	  
-        	$(data).each(function(){
-			   	console.log(this);
 
-				if (this.Nome.search(regex) != -1){
-					output += '<div class="col-md-6 well">';
-					output += '<div class="col-md-3"><img class="img-responsive" src="" /></div>';
-					output += '<div class="col-md-7">';
-					output += '<h5>' + this.Nome + '</h5>';
-					output += '<p>' + this.Código + '</p>'
-					output += '</div>';
-					output += '</div>';
-				}
-			});
-		
-		output += '</div>';
-		$('#filter-records').html(output);
+	$.getJSON('../quadros.json', function(dados){
+		// datas são seus dados do json
+		console.log(dados);
+ 
+// para pegar os valores pelos indices do objeto basta varer o objeto
+ 
+		$.each(dados, function(catalogo, resultado){
+			console.log(resultado.indice); // ta ai seus valores
 		});
+ 
 	});
+
+$('#txt-search').keyup(function(){
+	var searchField = $(this).val();
+	if(searchField === '')  {
+		$('#filter-records').html('');
+		return;
+	}
+	
+	var regex = new RegExp(searchField, "i");
+	var output = '<div class="row">';
+	var count = 1;
+	$.get(server, function(data) {
+	  for (z in data) {
+	  	var bla = data[z];
+	  	for (y in bla){
+	  		var oi = bla[y].Nome;
+	  		console.log(oi);
+	  		if (oi.search(regex) != -1){
+			  output += '<div class="col-md-6 well">';
+			  output += '<div class="col-md-3"><img class="img-responsive" src="'+bla[y].Imagem+'" alt="'+ oi +'" /></div>';
+			  output += '<div class="col-md-7">';
+			  output += '<h5>' + oi + '</h5>';
+			  output += '<p>' + bla[y].Preço1 + '</p>'
+			  output += '</div>';
+			  output += '</div>';
+			  if(count%2 == 0){
+				output += '</div><div class="row">'
+			  }
+			  count++;
+			}
+	  	}
+	  }
+	  output += '</div>';
+	  $('#filter-records').html(output);
 });
+});
+
+	});
